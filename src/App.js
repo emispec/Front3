5f3720e9534773847778a0cc8643498374c2552d
emispec <emisebastian86@gmail.com>
2022-11-08 17:16:11 -0300

    entrega parcial

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Estatus from "./components/Estatus.js";
+import Posteos from "./components/Posteos.js";
+
+// El componente App es el padre de:
+// - Estatus
+// - Posteos
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de likes.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Post.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos para que manipulen o lean su estado.
+
+function App() {
+  const [totalLikes, setTotalLikes] = useState(0);
+
+  function addTotalLikes() {
+    setTotalLikes(totalLikes + 1);
+  }
+
+  return (
+    <div className="App">
+      <Estatus totalLikes={totalLikes} />
+      <Posteos addTotalLikes={addTotalLikes} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Posteos.js
@@ -0,0 +1,24 @@
+import React from "react";
+import data from "./data.json";
+import Post from "./Post";
+// Debemos importar el listado de objetos del archivo JSON para usarlos como array a la hora de crear cada Item.
+// El componente Posteos es el padre de:
+// - Post (multiplicados por lo que haya en el listado JSON)
+// ESTADO: Posteos no necesita manejar un estado.
+// MÉTODOS: Posteos no requiere de métodos.
+// PROPS: Posteos recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos junto con el resto de las props que necesitan.
+
+export default function Posteos(props) {
+  return (
+    <div className="container">
+      {data.map((post) => (
+        <Post
+          key={post.id}
+          title={post.titulo}
+          content={post.texto}
+          addTotalLikes={props.addTotalLikes}
+        />
+      ))}
+    </div>
+  );
+}
